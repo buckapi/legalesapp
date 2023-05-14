2eccf4258df452e40f83ee4858a4b68039d7e730
buckapi <buckapis@gmail.com>
2023-05-14 09:14:01 -0500

    first commit

---
+++ src/app/services/script.store.ts
@@ -0,0 +1,32 @@
+
+ interface Scripts {
+    name: string;
+    src: string;
+}  
+export const ScriptStore: Scripts[] = [
+
+  {name: 'jquery', src: './assets/legales1/js/jquery.min.js'}, 
+     {name: 'tether', src: './assets/legales1/js/tether.min.js'}, 
+     {name: 'bootstrap', src: './assets/legales1/js/bootstrap.min.js'},  
+     {name: 'easing', src: './assets/legales1/js/jquery.easing.js'}, 
+     {name: 'waypoints', src: './assets/legales1/js/jquery-waypoints.js'}, 
+     {name: 'validate', src: './assets/legales1/js/jquery-validate.js'}, 
+     {name: 'prettyPhoto', src: './assets/legales1/js/jquery.prettyPhoto.js'}, 
+     {name: 'slick', src: './assets/legales1/js/slick.min.js'}, 
+     {name: 'numinate', src: './assets/legales1/js/numinate.min.js'}, 
+     {name: 'main', src: './assets/legales1/js/main.js'},
+
+     {name: 'tools', src: './assets/legales1/revolution/js/jquery.themepunch.tools.min.js'}, 
+     {name: 'revolution', src: './assets/legales1/revolution/js/jquery.themepunch.revolution.min.js'}, 
+     {name: 'slider', src: './assets/legales1/revolution/js/slider.js'}, 
+
+     {name: 'actions', src: './assets/legales1/revolution/js/extensions/revolution.extension.actions.min.js'}, 
+     {name: 'carousel', src: './assets/legales1/revolution/js/extensions/revolution.extension.carousel.min.js'}, 
+     {name: 'kenburn', src: './assets/legales1/revolution/js/extensions/revolution.extension.kenburn.min.js'}, 
+     {name: 'layeranimation', src: './assets/legales1/revolution/js/extensions/revolution.extension.layeranimation.min.js'}, 
+     {name: 'migration', src: './assets/legales1/revolution/js/extensions/revolution.extension.migration.min.js'}, 
+     {name: 'navigation', src: './assets/legales1/revolution/js/extensions/revolution.extension.navigation.min.js'}, 
+     {name: 'parallax', src: './assets/legales1/revolution/js/extensions/revolution.extension.parallax.min.js'}, 
+     {name: 'slideanims', src: './assets/legales1/revolution/js/extensions/revolution.extension.slideanims.min.js'}
+
+];
